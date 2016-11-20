dccd89e4d98f2ea2b073f82ecfbee1aa87126bd3
CaptainDaVinci <yashkothari199767@gmail.com>
2016-11-20 19:11:31 +0530

    Added frequency of an element in an array C code.

---
+++ frequency.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    int n, count;
+    int arr[100], freq[100];
+    printf("Enter the size of the array : ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array : ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        freq[i] = -1;
+    }
+    // Calculating the frequency.
+    for(i = 0; i < n; i++)
+    {
+        count = 1;
+        if (freq[i] != 0)
+        {
+            for ( j = i + 1; j < n; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    freq[j] = 0;
+                    count++;
+                }
+            }
+                freq[i] = count;
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if (freq[i] != 0)
+        {
+            printf("Element : %d, Frequency : %d\n", arr[i], freq[i]);
+        }
+    }
+    return 0;
+}
