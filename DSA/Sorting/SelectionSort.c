a609e6f31d5c8d5ac3876e5bab328ece6a243e9c
CaptainDaVinci <yashkothari199767@gmail.com>
2017-02-15 23:10:46 +0530

    Code improved

--- DSA/Sorting/SelectionSort.c
+++ DSA/Sorting/SelectionSort.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void selectionSort(int arr[], int n);
 
 int main(void)
 {
+    clock_t start, end;
+    start = clock();
+
     int n, i, imin, temp, j;
 
     printf("Enter the size of the array : ");
@@ -10,18 +16,34 @@ int main(void)
 
     int *arr = (int *)malloc(n*sizeof(int));
 
-    printf("Enter the elements of the unsorted array : ");
+    printf("Enter the elements of the unsorted array: ");
 
     for (i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    // Selection Sort implementation.
+    selectionSort(arr, n);
+    printf("\nThe sorted array is: ");
+
     for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
+    free(arr);
+
+    end = clock();
+    printf("Sorted in %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+}
+
+void selectionSort(int arr[], int n)
+{
+    int imin, i, j, temp;
+
+    for(i = 0; i < n; i++)
     {
         imin = i;
-        for (j = i +1; j < n; j++)
+        for(j = i + 1; j < n; j++)
         {
-            if (arr[j] < arr[imin])
+            if(arr[j] < arr[imin])
                 imin = j;
         }
 
@@ -29,11 +51,4 @@ int main(void)
         arr[i] = arr[imin];
         arr[imin] = temp;
     }
-
-    printf("\nThe sorted array is : ");
-
-    for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-
-    free(arr);
 }
