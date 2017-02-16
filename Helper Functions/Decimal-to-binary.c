93fd10816c550d5f9be0e69007be6f72974c4909
CaptainDaVinci <yashkothari199767@gmail.com>
2017-02-16 19:10:35 +0530

    Added float to binary conversion

--- Helper Functions/Decimal-to-binary.c
+++ Helper Functions/Decimal-to-binary.c
@@ -1,30 +1,117 @@
-#include <stdio.h>
+/*
+ *  Possibly Over-engineered.
+ *
+ *      :P
+ *
+ */
 
-int convert(int binaryNum);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define PRECISION 10
+
+void convertFloatPart(char binary[], float num);
+void convertIntPart(char binary[], int binaryNum);
+void reverse(char binary[]);
+
+int i;
 
 int main(void)
 {
-    int n;
+    float num;
+    char *binary = calloc(1000, sizeof(int));
+    int i;
 
     printf("Enter the number: ");
-    scanf("%d", &n);
+    scanf("%f", &num);
 
-    printf("Binary - %08d\n", convert(n));
+    convertIntPart(binary, (int)num);
+    convertFloatPart(binary, num);
+
+    printf("Binary: %s\n", binary);
+    free(binary);
 }
 
-int convert(int n)
+void convertFloatPart(char binary[], float num)
 {
+    bool padded = false;
+    int digits = 0;
+    int padding;
+    int intPart = num;
+    float floatPart = num - (float)intPart;
+
+    while(floatPart != 0.0f && digits < PRECISION)
+    {
+        floatPart *= 2;
+        intPart = floatPart;
+        binary[i++] = intPart + '0';
+        digits++;
+
+        if(digits % 4 == 0)
+            binary[i++] = ' ';
+
+        floatPart -= intPart;
+    }
+
+    for(padding = 4 - digits % 4; padding >  0 && padding < 4; padding--)
+    {
+        padded = true;
+        binary[i++] = '0';
+    }
+
+    if(padded)
+        binary[i++] = ' ';
+
+    binary[i] = '\0';
+    strcat(binary, "0000");
+}
+
+
+void convertIntPart(char binary[], int n)
+{
+    bool padded = false;
+    int padding;
     int rem;
-    int i = 1;
-    int bin = 0;
+    int digits = 0;
 
     while(n != 0)
     {
         rem = n % 2;
         n /= 2;
-        bin = bin + rem * i;
-        i = i * 10;
+        binary[i++] = rem + '0';
+        digits++;
+
+        if(digits % 4 == 0)
+            binary[i++] = ' ';
     }
 
-    return bin;
+    for(padding = 4 - digits % 4; padding >  0 && padding < 4; padding--)
+    {
+        padded = true;
+        binary[i++] = '0';
+    }
+
+    if(padded)
+        binary[i++] = ' ';
+
+    strcat(binary, "0000.");
+    i += 5;
+
+    reverse(binary);
+}
+
+void reverse(char binary[])
+{
+    int j = 0;
+    int k = 0;
+    char temp;
+
+    for(j = 0, k = i - 2; j < k; j++, k--)
+    {
+        temp = binary[j];
+        binary[j] = binary[k];
+        binary[k] = temp;
+    }
 }
